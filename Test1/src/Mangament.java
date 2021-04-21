1704f779753ae802d2310af4a42b0aa7fd87ac17
@Ritu-Yadav28 <ritu28198yadav@gmail.com>
2021-04-21 12:34:17 +0530

    Third commit

---
+++ Test1/src/Mangament.java
@@ -0,0 +1,10 @@
+
+public class Mangament {
+	public void depart() {
+		System.out.println("this is Managment Department");
+	}
+    public static void main(String[]args) {
+    	Mangament m= new Mangament();
+    	m.depart();
+    }
+}
