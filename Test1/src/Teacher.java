4d5d876e9122a93a25190bed5f1d60df324df172
@Ritu-Yadav28 <ritu28198yadav@gmail.com>
2021-04-21 12:24:38 +0530

    second commit

---
+++ Test1/src/Teacher.java
@@ -0,0 +1,17 @@
+
+public class Teacher {
+	  public String subject;
+	  public int totalTeacher;
+	  public Teacher(String s,int i) {
+		  this.subject =s;
+		  this.totalTeacher=t;
+		  
+	  }
+
+	public static void main(String[]args) {
+		System.out.println("Teacher's report");
+		Teacher sub1= new Teacher("science",10);
+		Teacher sub2= new Teacher("English",3);
+		Teacher sub3 =new Teacher("Hindi",4);
+	}
+}
